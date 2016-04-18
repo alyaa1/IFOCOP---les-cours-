d746777ea5f5bdc7f396f5f76815d6ea90628fa6
loranm <chmoxster@gmail.com>
2016-04-18 12:39:41 +0200

    exercice 16 Animation du sprite gauche et droite fonctionnelle

--- Javascript DOM/Exercices_faits/4 - Le modele objet du document/exercice16.js
+++ Javascript DOM/Exercices_faits/4 - Le modele objet du document/exercice16.js
@@ -9,12 +9,12 @@ Génère le tableau de sprite avec les
 arguments spritePerRow et rows
 ******************************************/
 
-var spriteSheet = function(col,rows){
-  var nbFrames = col * rows;
+var spriteSheet = function(col,rows,missingCols){
+  var nbFrames = (col * rows) - missingCols ;
   while(sprites.length <= nbFrames){
     for (var sprite = 0; sprite < nbFrames; sprite++){
       var positionLeft = -130 * (sprite%col);
-      var positionTop = -155 * Math.floor(sprite/col);
+      var positionTop = -152 * Math.floor(sprite/col);
       sprites.push([positionLeft,positionTop]);
     };
     return sprites;
@@ -27,14 +27,20 @@ function moveSprite(sprite,sprites,compteur){
       sprite.style.top = sprites[i][1] + 'px';
   };
 
-
+//fonction compteur
 var compteur = function(){
   var i = 0;
   return function(){
+    if (i >= sprites.length){i = 1;}
     return i++;
   };
 };
-
 var g = compteur();
 
-//il faudrait faire une closure pour faire évoluer le compteur
+
+spriteSheet(7,4,1);
+
+/*pour retourner une image
+sprite.style.transform('scaleX(-1)');
+sprite.style.left('-885px')
+pas de 130px*/
---
+++ Javascript DOM/Exercices_faits/4 - Le modele objet du document/exercice16_v3.js
@@ -0,0 +1,91 @@
+'use strict';
+
+
+/***************************************************
+fonction compteur
+**************************************************/
+// var compteur = function(){
+//   var i = 0;
+//   return function(){
+//     if (i >= lapin.framesPositions.length){i = 0;}
+//     return i++;
+//   };
+// };
+// var g = compteur();
+/**************************************************/
+
+
+/******************************************
+Génère le tableau de sprite avec les
+arguments spritePerRow et rows
+******************************************/
+
+var AnimatedSpriteConstructor = function(spriteSheetId, imageWidth, spriteWidth, spriteHeight, col, rows, missingCols){
+  this.imageWidth = imageWidth;
+  this.width = spriteWidth;
+  this.height = spriteHeight;
+  this.SpriteSheet = document.getElementById(spriteSheetId);
+  this.col = col;
+  this.rows = rows;
+  this.missingCols = missingCols;
+  this.nbFrames = (col * rows) - missingCols;
+  this.framesPositions = [];
+  this.framesPositionsLeft = [];
+  this.createFramesPositionsRight = function(){
+    while (this.framesPositions.length <= this.nbFrames){
+      for (var i = 1; i < this.nbFrames; i++){
+        var positionLeft = this.width * (i % this.col);
+        var positionTop = this.height * Math.floor(i/this.col);
+        this.framesPositions.push([positionLeft,positionTop]);
+      };
+    };
+  };
+this.counter = 0;
+};
+
+
+
+/*******************************************************
+//créer un objet avec ces paramètres dans l'ordre suivant
+  spriteSheetId,imageWidth, spriteWidth, spriteHeight, col,rows,missingCols
+  *****************************************************/
+var lapin = new AnimatedSpriteConstructor('contenu',1024,130, 152, 7,4,1);
+console.log(lapin);
+lapin.createFramesPositionsRight();
+
+
+
+/****************************************************
+Animation du sprite
+****************************************************/
+function animateSpriteRight(sprite){
+  sprite.SpriteSheet.style.transform = 'scaleX(1)';
+  // sprite.SpriteSheet.style.left = '0px';
+  sprite.SpriteSheet.style.left = '-' + sprite.framesPositions[sprite.counter][0] + 'px';
+  sprite.SpriteSheet.style.top = '-' + sprite.framesPositions[sprite.counter][1] + 'px';
+  sprite.counter++;
+  if (sprite.counter >= sprite.nbFrames){
+    sprite.counter = 0
+  };
+};
+
+function animateSpriteLeft(sprite){
+  sprite.SpriteSheet.style.transform ='scaleX(-1)';
+  // sprite.SpriteSheet.style.left = sprite.imageWidth + 'px';
+  sprite.SpriteSheet.style.left = '-' + (sprite.imageWidth - sprite.framesPositions[sprite.counter][0] - sprite.width) + 'px';
+  sprite.SpriteSheet.style.top = '-' + sprite.framesPositions[sprite.counter][1] + 'px';
+  sprite.counter++
+  if (sprite.counter >= sprite.nbFrames){
+    sprite.counter = 0
+  };
+};
+
+/******************************************************
+Deplacement de la div
+*******************************************************/
+var moveDivRight = function(){
+  maDiv = document.getElementById('container');
+  return maDiv;
+
+
+}
